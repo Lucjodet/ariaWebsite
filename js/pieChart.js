d82e5c1b292e7416bb9691e81360505461609160
Luc Jodet <lucjodet@gmail.com>
2020-09-28 01:42:30 +0200

    Add files via upload

---
+++ js/pieChart.js
@@ -0,0 +1,109 @@
+// Build the chart
+Highcharts.chart('container2', {
+  chart: {
+      plotBackgroundColor: '#000000',
+      plotBorderWidth: null,
+      plotShadow: false,
+      backgroundColor: '#000000',
+      type: 'pie',
+      style: {
+        font: '16px "Raleway", Verdana, sans-serif',
+        textColor: '#FFFFFF'
+     },
+      events: {
+        load: function(event) {
+            var total = 0; // get total of data
+            for (var i = 0, len = this.series[0].yData.length; i < len; i++) {
+                total += this.series[0].yData[i];
+            }
+            var text = this.renderer.text(
+                'Total: ' + total + 'M',
+                this.plotLeft,
+                this.plotTop - 20
+            ).attr({
+                zIndex: 5
+            }).add()
+    }
+  }
+},
+
+credits: {
+    enabled: false
+},
+
+  title: {
+    text: 'Aria Distribution',
+    style: {
+      color:'#FFFFFF',
+      font: 'bold 16px "Raleway", Verdana, sans-serif'
+  },
+},
+
+subtitle: {
+  text: '',
+  style: {
+    color:'#FFFFFF'
+ },
+  align: 'center',
+  verticalAlign: 'middle'
+},
+
+  colors: [
+    '#B3914B',
+    '#99793F',
+    '#725A30',
+    '#4C3C20',
+    '#302615',
+    '#110D06'
+],
+
+  tooltip: {
+      pointFormat: '{series.name}: <b>{point.y}M</b>'
+  },
+  accessibility: {
+      point: {
+          valueSuffix: '%'
+      }
+  },
+  plotOptions: {
+      pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage}%',
+            color:'#FFFFFF'
+          },
+          innerSize: '50%',
+          showInLegend: false
+      }
+  },
+  series: [{
+      name: 'aria',
+      colorByPoint: true,
+      data: [{
+          name: 'Private Sale',
+          y: 45,
+          sliced: true,
+          selected: true
+      }, {
+          name: 'Public Sale',
+          y: 25
+      }, {
+          name: 'Team',
+          y: 34
+      }, {
+          name: 'Advisor',
+          y: 6
+      }, {
+          name: 'Community incentives',
+          y: 10
+      }, {
+          name: 'Liquid reserve',
+          y: 10
+      }, {
+          name: 'Long-term Reserve',
+          y: 70
+      }]
+  }]
+});
---
+++ js/stackedChart.js
@@ -0,0 +1,91 @@
+Highcharts.chart('container1', {
+  chart: {
+    type: 'area',
+    backgroundColor: '#000000',
+    style: {
+      font: '16px "Raleway", Verdana, sans-serif'
+   }
+  },
+  credits: {
+    enabled: false
+  },
+  title: {
+    text: 'Aria Lockup Schedule',
+    style: {
+      color:'#FFFFFF',
+      font: 'bold 16px "Raleway", Verdana, sans-serif'
+   }
+  },
+  colors: [
+    '#B3914B',
+    '#99793F',
+    '#725A30',
+    '#4C3C20',
+    '#302615',
+    '#110D06'
+],
+  subtitle: {
+    style: {
+      color:'#FFFFFF'
+   }
+  },
+  xAxis: {
+    categories: ['2020', '2021', '2022', '2023', '2024', '2025', '2026'],
+    tickmarkPlacement: 'on',
+    title: {
+      enabled: false
+    },
+    labels: {
+      style: {
+         color:'#FFFFFF'
+      },
+    },
+  },
+  yAxis: {
+    title: {
+      text: 'aria',
+      style: {
+        color:'#FFFFFF'
+     }
+    },
+    labels: {
+      formatter: function () {
+        return this.value ;
+      },
+      style: {
+        color:'#FFFFFF'
+     },
+    }
+  },
+  tooltip: {
+    split: true,
+    valueSuffix: ' aria'
+  },
+  plotOptions: {
+    area: {
+      stacking: 'normal',
+      lineColor: '#FFFFFF',
+      lineWidth: 1,
+      marker: {
+        lineWidth: 1,
+        lineColor: '#FFFFFF'
+      }
+    }
+  },
+  series: [{
+    name: 'Public Sale',
+    data: [5, 10, 15, 20, 20, 20, 20]
+  }, {
+    name: 'Private sale',
+    data: [10, 15, 20, 25, 30, 35, 40]
+  }, {
+    name: 'Team',
+    data: [0, 0, 5,10, 15, 20, 25]
+  }, {
+    name: 'Advisors',
+    data: [0, 3, 6, 6, 6, 6, 6]
+  }, {
+    name: 'Liquid Reserve',
+    data: [10, 10, 10, 10, 10, 10, 10]
+  }],
+});
